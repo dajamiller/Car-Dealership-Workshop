14a92647df3f5178eac60d65e577415393641365
Daja Miller <dajamiller96@gmail.com>
2025-10-30 13:55:28 -0700

    overridden methods implemented

--- src/main/java/com/pluralsight/LeaseContract.java
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -2,4 +2,20 @@ package com.pluralsight;
 
 public class LeaseContract extends Contract {
 
+    public LeaseContract(String date, String customerName, String customerEmail, boolean vehicleSold) {
+        super(date, customerName, customerEmail, vehicleSold);
+    }
+
+    //
+//
+//    these values will be computed based on type of contract
+    @Override
+    public double getTotalPrice() {
+        return 0;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        return 0;
+    }
 }
--- src/main/java/com/pluralsight/SaleContract.java
+++ src/main/java/com/pluralsight/SaleContract.java
@@ -2,12 +2,12 @@ package com.pluralsight;
 
 public class SaleContract extends Contract {
 
-    double salesTax = 0.5;
-    int recordingFee = 100;
-    int processingFee = 295;    //if under 10k, if over will be $495
-    boolean isFinanced;
+    private double salesTax = 0.5;
+    private int recordingFee = 100;
+    private int processingFee = 295;    //if under 10k, if over will be $495
+    private boolean isFinanced;
     //if isFinanced
-    double monthlyPayment;  //loan = 4.25% interest for 48 months if over $10k, otherwise 5.25% for 24 months
+    private double monthlyPayment;  //loan = 4.25% interest for 48 months if over $10k, otherwise 5.25% for 24 months
 
 
     public SaleContract(String date, String customerName, String customerEmail, boolean vehicleSold, double salesTax, int recordingFee, int processingFee, boolean isFinanced) {
@@ -25,6 +25,7 @@ public class SaleContract extends Contract {
     public double getTotalPrice() {
 //        Vehicle.vehiclePrice
 //        return (vehiclePrice * salesTax) + recordingFee + processingFee;
+        return 0;
     }
 
     @Override
